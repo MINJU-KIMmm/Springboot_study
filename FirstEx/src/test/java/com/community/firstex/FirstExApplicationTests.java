fe7f681ee55446e6f73e800c9f7b3fa508dfb193
MINJU_KIMmm <0112km@ewhain.net>
2021-06-24 17:34:36 +0900

    z

---
+++ FirstEx/src/test/java/com/community/firstex/FirstExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.firstex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
